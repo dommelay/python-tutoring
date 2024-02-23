ebaf49b18664fdef6491d62acb93ca29ae94670a
Dominique Clark <melaydominique@gmail.com>
2024-02-23 15:06:05 -0800

    histogras and scatterplots, matplotlib & sns

--- lecture4.py
+++ lecture4.py
@@ -1,16 +1,33 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 #1
 df = pd.read_csv('/Users/dominiqueclark/ENGINEER/python_projects/python-tutoring/Inequality in Income 2 (1).csv')
 
 #2
 #first few rows
-print(df.head())
+# print(df.head())
 #info about data, data types and missing values
-print(df.info())
+# print(df.info())
 #descriptive statistics for numerical columns
-print(df.describe())
+# print(df.describe())
+
+#3
+#histogram to to visualize the income distribution
+# plt.figure()
+# sns.histplot(df['Inequality in income (2010)'])
+# plt.title('Income Distribution')
+# plt.xlabel('Income')
+# plt.ylabel('Frequency')
+# plt.show()
+#scatter plots to visualize the relationship between variable pairs.
+# sns.pairplot(df)
+# plt.title('relationship between variable pais')
+# plt.show()
+
+
 
 
 
